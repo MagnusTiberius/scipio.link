79732386e91de67a7705b64588f948b48bbb9977
magnustiberius <bgonzales20001@gmail.com>
2021-03-24 13:17:00 -0700

    golang setup centos

--- misc/setup_golang.sh
+++ misc/setup_golang.sh
@@ -1,3 +1,16 @@
+#!/bin/sh
 
+mkdir -p ~/projects/{bin,pkg,src}
 
-//https://golang.org/dl/go1.16.2.linux-amd64.tar.gz
+curl -LO https://golang.org/dl/go1.16.2.linux-amd64.tar.gz
+
+sudo tar -C /usr/local -xvzf go1.16.2.linux-amd64.tar.gz
+
+//sudo vi /etc/profile.d/path.sh
+//export PATH=$PATH:/usr/local/go/bin
+
+//vi ~/.bash_profile
+//export GOBIN="$HOME/projects/bin"
+//export GOPATH="$HOME/projects/src"
+
+//go install $GOPATH/hello.go
