9fbda50b38b25fb9243fcd66d8d8219614030285
magnustiberius <bgonzales20001@gmail.com>
2021-03-24 21:02:25 -0700

    demo1

--- Golang/helloworld.go
+++ Golang/helloworld.go
@@ -4,13 +4,29 @@ import (
     "fmt"
     "log"
     "net/http"
+	"html/template"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+func renderTemplate(w http.ResponseWriter, r *http.Request) {
+    student := Student{
+        Name:       "GB",
+        College:    "GolangBlogs",
+        RollNumber: 1,
+    }
+    parsedTemplate, _ := template.ParseFiles("templates/demo1.html")
+    err := parsedTemplate.Execute(w, student)
+    if err != nil {
+        log.Println("Error executing template :", err)
+        return
+    }
+}
+
 func main() {
-    http.HandleFunc("/", handler)
+    //http.HandleFunc("/", handler)
+	http.HandleFunc("/", renderTemplate)
     log.Fatal(http.ListenAndServe(":80", nil))
 }
